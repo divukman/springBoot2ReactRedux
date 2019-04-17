e61248609874f5f3dea7ff2bf2073398c0cc9823
divukman <divukman@gmail.com>
2019-04-17 16:47:12 +0200

    Object validation

--- ppmtool/src/main/java/com/dimitar/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/dimitar/ppmtool/domain/Project.java
@@ -1,9 +1,12 @@
 package com.dimitar.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -15,13 +18,27 @@ public class Project {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank (message = "Project name is required")
 	private String projectName;
+
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+
+	@NotBlank(message = "Project description is required")
 	private String description;
+
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 
 
