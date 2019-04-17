af8762c818af58048155068a3d8ee1d3547e6c8d
divukman <divukman@gmail.com>
2019-04-17 17:02:52 +0200

    Validation and binding result validation added

--- ppmtool/src/main/java/com/dimitar/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/dimitar/ppmtool/web/ProjectController.java
@@ -5,7 +5,10 @@ import com.dimitar.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,7 +18,12 @@ public class ProjectController {
 	ProjectService projectService;
 
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProjet(@RequestBody Project project) {
+	public ResponseEntity<?> createNewProjet(@Valid @RequestBody Project project, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST) ;
+		}
+
 		final Project savedProject = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
 	}
