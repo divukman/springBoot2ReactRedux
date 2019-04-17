7e3f117157da9fb97591250500f73ce3837a197b
divukman <divukman@gmail.com>
2019-04-17 15:34:09 +0200

    Adding repository

--- ppmtool/src/main/java/com/dimitar/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/dimitar/ppmtool/domain/Project.java
@@ -1,4 +1,38 @@
 package com.dimitar.ppmtool.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String projectName;
+	private String projectIdentifier;
+	private String description;
+	private Date start_date;
+	private Date end_date;
+
+	private Date created_At;
+	private Date updated_At;
+
+
+	@PrePersist
+	protected void onCreate() {
+		this.created_At = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updated_At = new Date();
+	}
+
 }
