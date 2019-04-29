73da8b8ddc8e86588954b03bcc8775f1f16ba222
divukman <divukman@gmail.com>
2019-04-29 13:47:40 +0200

    validation markup

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem';
-import CreateProjectButton from './Project/CreateProjectButton';
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
 
 class Dashboard extends Component {
   render() {
     return (
-        <div className="projects">
+      <div className="projects">
         <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                    <CreateProjectButton />
-                    <br />
-                    <hr />
-                    
-                    <ProjectItem />
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
 
-                </div>
+              <ProjectItem />
             </div>
+          </div>
         </div>
-    </div>
+      </div>
     );
   }
 }
