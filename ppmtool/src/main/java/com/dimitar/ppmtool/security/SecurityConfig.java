5fb9a98e5d4c6088fb70f0811af309f74f4604bf
divukman <divukman@gmail.com>
2019-05-16 11:33:19 +0200

    backend security work

---
+++ ppmtool/src/main/java/com/dimitar/ppmtool/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.dimitar.ppmtool.security;
+
+import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .headers().frameOptions().sameOrigin().and()  //enable h2 db, allow console
+                .authorizeRequests()
+              //  .antMatchers("/", "/**/*.png").permitAll() // for future
+                .anyRequest().authenticated();
+    }
+}
