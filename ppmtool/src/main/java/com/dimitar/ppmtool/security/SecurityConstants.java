da1531e59ae92c42a45e318714fc0bfa2f13eed3
divukman <divukman@gmail.com>
2019-05-21 10:10:58 +0200

    tested code

--- ppmtool/src/main/java/com/dimitar/ppmtool/security/SecurityConstants.java
+++ ppmtool/src/main/java/com/dimitar/ppmtool/security/SecurityConstants.java
@@ -6,5 +6,5 @@ public class SecurityConstants {
     public static final String SECRET =  "U2VjcmV0S2V5VG9HZW5lcmF0ZUpXVHM=";
     public static final String TOKEN_PREFIX = "Bearer "; // Space at the end needed!
     public static final String HEADER_STRING = "Authorization";
-    public static final Long EXPIRATION_TIME = 30_000L;
+    public static final Long EXPIRATION_TIME = (long)(24 * 60 * 60 * 1000);
 }
